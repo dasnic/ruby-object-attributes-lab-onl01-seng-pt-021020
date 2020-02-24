d66b9cb6d27421811a18483e324ee349acaf4341
Lydell Gaines <dasnic70@gmail.com>
2020-02-24 03:19:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,18 @@
-def name=(name)
+class Person
+
+  def name=(name)
     @name = name
   end
-  
   def name
     @name
   end
 
   def job=(job)
     @job = job
-  end 
+  end
 
-  def job 
+  def job
     @job
-  end 
+  end
+end
   
